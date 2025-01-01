608ec976bba60fd33b098336b815fc5e432eab00
inksne <debanyj23@gmail.com>
2025-01-01 19:24:39 +0300

    add API tests

--- lib/books/tests.py
+++ lib/books/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SimpleTests(TestCase):
+    def test_main_page(self):
+        response = self.client.get(reverse('base'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_us_page(self):
+        response = self.client.get(reverse('about_us'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_page(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
